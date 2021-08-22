39e1bafd4b33bef51741704ccdc446d249b90ae5
DeForestt Thompson <deforesttthompson@gmail.com>
2021-08-22 18:40:50 -0400

    added Ident to Declare tostring output

--- src/AST.cpp
+++ src/AST.cpp
@@ -5,3 +5,7 @@ std::string AST::Sequence::toString(){
     std::string output = this->Statment1->toString() + this->Statment2->toString();
     return output;
 }
+
+std::string AST::Declare::toString(){
+    return "Declare" + this->Ident; 
+};
