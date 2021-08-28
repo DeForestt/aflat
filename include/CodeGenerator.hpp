696022f2b73448019c7ccf36bb40525b369a7059
DeForestt Thompson <deforesttthompson@gmail.com>
2021-08-28 19:00:20 -0400

    commit

--- include/CodeGenerator.hpp
+++ include/CodeGenerator.hpp
@@ -16,7 +16,7 @@ namespace gen{
     class CodeGenerator{
         private:
         links::LinkedList<Symbol> SymbolTable;
-        std::string intArgs[6] = {"rdi", "rsi", "rdx", "rcx", "r8", "r9"};
+        std::string intArgs[6] = {"%rdi", "%rsi", "%rdx", "%rcx", "%r8", "%r9"};
         int intArgsCounter;
         std::string GenExpr(AST::Expr * expr);
         
