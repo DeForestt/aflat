f22fb95a3079b4334655e35995a398c9b762efc2
DeForestt Thompson <deforesttthompson@gmail.com>
2021-09-15 23:04:18 -0400

    anm

--- src/ASM.cpp
+++ src/ASM.cpp
@@ -93,7 +93,7 @@ std::string ASMC::Je::toString(){
 }
 
 std::string ASMC::Jl::toString(){
-    return "\tjl\t" + this->to + "\n";
+    return "\tjle\t" + this->to + "\n";
 }
 
 std::string ASMC::Jg::toString(){
