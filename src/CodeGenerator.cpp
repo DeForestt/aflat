07b4b64db1be620af69670eb368af19830f84bfb
DeForestt Thompson <deforesttthompson@gmail.com>
2021-09-17 15:09:31 -0400

    pop

--- src/CodeGenerator.cpp
+++ src/CodeGenerator.cpp
@@ -319,7 +319,10 @@ ASMC::File gen::CodeGenerator::GenSTMT(AST::Statment * STMT){
         ASMC::File file = this->GenSTMT(func->statment);
         OutputFile << file;
         
+        int alligne = 0;
+        if(this->SymbolTable.head != nullptr){
         int alligne = ((this->SymbolTable.peek().byteMod + 15) / 16) * 16;
+        }
         ASMC::Sub * sub = new ASMC::Sub;
         sub->op1 = "$" + std::to_string(alligne);
         sub->op2 = this->registers["%rsp"]->qWord;
