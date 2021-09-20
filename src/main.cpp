5d10e7400c3aa94611c81f7c1552d437f024afd9
DeForestt Thompson <deforesttthompson@gmail.com>
2021-09-20 15:37:29 -0400

    clear

--- src/main.cpp
+++ src/main.cpp
@@ -11,48 +11,50 @@
 int main(int argc, char *argv[])
 {
     try{
-    lex::Lexer scanner;
-    links::LinkedList<lex::Token* > tokens;
+        lex::Lexer scanner;
+        links::LinkedList<lex::Token* > tokens;
 
-    std::ifstream ifs(argv[1]);
-    std::string content( (std::istreambuf_iterator<char>(ifs) ),
-                       (std::istreambuf_iterator<char>()    ) );
+        std::ifstream ifs(argv[1]);
+        std::string content( (std::istreambuf_iterator<char>(ifs) ),
+                        (std::istreambuf_iterator<char>()    ) );
 
-    try{
-        tokens = scanner.Scan(content);
-    }catch (int x){
-        std::cout << " unparsable Char at index " + x;
-        return 0;
-    }
-    tokens.invert();
-    parse::Parser parser;
+        try{
+            tokens = scanner.Scan(content);
+        }catch (int x){
+            std::cout << " unparsable Char at index " + x;
+            return 0;
+        }
+        tokens.invert();
+        parse::Parser parser;
 
-    AST::Statment * Prog = parser.parseStmt(tokens);
-    
-    gen::CodeGenerator genny;
-    ASMC::File file = genny.GenSTMT(Prog);
-    file.text.invert();
+        AST::Statment * Prog = parser.parseStmt(tokens);
+        
+        gen::CodeGenerator genny;
+        ASMC::File file = genny.GenSTMT(Prog);
+        file.text.invert();
 
-   file.data.invert();
-    file.linker.invert();
+    file.data.invert();
+        file.linker.invert();
 
-    while(file.linker.head != nullptr){
-        std::cout << file.linker.pop()->toString();
-    }
+        std::cout << "\n\n.text\n\n";
 
+        std::ofstream ofs;
 
+        if (argc == 1) ofs.open("out.s"); else ofs.open(argv[2]);
 
-    std::cout << "\n\n.text\n\n";
+        while(file.linker.head != nullptr){
+            std::cout << file.linker.pop()->toString();
+        }
 
-    while(file.data.head != nullptr){
-        std::cout << file.data.pop()->toString();
-    }
+        while(file.data.head != nullptr){
+            ofs << file.data.pop()->toString();
+        }
 
-    while(file.text.head != nullptr){
-        std::cout << file.text.pop()->toString();
-    }
-    }
-    catch(err::Exception e){
+        while(file.text.head != nullptr){
+            ofs << file.text.pop()->toString();
+        }
+
+    }catch(err::Exception e){
         std::cout << std::endl << "Exception: " << e.errorMsg << std::endl << std::endl;
     }
     return 0;
