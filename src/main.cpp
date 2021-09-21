09b5ffef8000f110052d72d32748c163b21ceb1b
DeForestt Thompson <deforesttthompson@gmail.com>
2021-09-20 20:00:27 -0400

    now you can set the root dir for your libraries

--- src/main.cpp
+++ src/main.cpp
@@ -81,7 +81,7 @@ std::string preProcess(std::string input){
                                 (std::istreambuf_iterator<char>()    ) );
                 ifs.close();
                 output += preProcess(content);
-            }else if(line.substr(0, 6) == ".root"){
+            }else if(line.substr(0, 5) == ".root"){
                 int startPos = line.find_first_of('\"') + 1;
                 int endPos = line.find_last_of('\"');
                 root = line.substr(startPos, endPos - startPos);
