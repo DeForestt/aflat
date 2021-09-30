020fa5c4dfdd0625556f0e8d642cba5ded44959d
DeForestt Thompson <deforesttthompson@gmail.com>
2021-09-30 19:10:17 -0400

    working on a template builder

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,6 @@ int main(int argc, char *argv[])
         std::string content( (std::istreambuf_iterator<char>(ifs) ),
                         (std::istreambuf_iterator<char>()    ) );
         ifs.close();
-        std::cout << "this is a test";
         try{
             tokens = scanner.Scan(preProcess(content));
         }catch (int x){
