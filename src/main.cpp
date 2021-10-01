80f7483004070a2f5e8846951803ad0039f16306
DeForestt Thompson <deforesttthompson@gmail.com>
2021-10-01 10:24:25 -0400

    working on a template builder

--- src/main.cpp
+++ src/main.cpp
@@ -115,7 +115,8 @@ std::string getExePath()
 }
 
 void buildTemplate(std::string value){
-    std::cout << getExePath().substr(0, s1.find_last_of("\\/"));
+    std::string filename = getExePath();
+    std::string exepath = filename.substr(0, filename.find_last_of("\\/"));
     std::filesystem::create_directories(value);
     std::filesystem::create_directories(value + "/src");
     std::filesystem::create_directories(value + "/head");
