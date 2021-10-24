0783ab6f2dc7b436513c8a84b80a4b76e6508321
DeForestt Thompson <deforesttthompson@gmail.com>
2021-10-24 00:06:19 -0400

    Added Collections Library with Stack

--- src/main.cpp
+++ src/main.cpp
@@ -238,9 +238,10 @@ void buildTemplate(std::string value){
     outfile <<filename << " std/src/io.af std/io.s\n";
     outfile <<  filename << " std/src/math.af std/math.s\n";
     outfile <<  filename << " std/src/strings.af std/strings.s\n";
+    outfile <<  filename << " std/src/Collections.af std/collections.s\n";
     outfile <<  filename << " std/src/files.af std/files.s\n";
     outfile <<  filename << " std/src/std.af std/std.s\n";
-    outfile << "gcc -O0 -g -no-pie out.s std/asm.s std/std.s std/io.s std/math.s std/strings.s std/files.s";
+    outfile << "gcc -O0 -g -no-pie out.s std/asm.s std/std.s std/io.s std/collections.s std/math.s std/strings.s std/files.s";
 
 }
 
