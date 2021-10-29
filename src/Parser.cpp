32787847641adb66eaab1f4c8a0fb22dc33c3a99
DeForestt Thompson <deforesttthompson@gmail.com>
2021-10-29 04:54:38 -0400

    use symbol in sted of opSymbol... should be one thing

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -539,22 +539,5 @@ AST::Expr* parse::Parser::parseExpr(links::LinkedList<lex::Token*> &tokens){
             return compound;
         }
     }
-    else if (dynamic_cast<lex::Symbol *>(tokens.peek()) != nullptr){
-        AST::Compound  * compound = new AST::Compound();
-        lex::Symbol sym = *dynamic_cast<lex::Symbol *>(tokens.pop());
-        if (sym.meta == "<"){
-            tokens.pop();
-            compound->op = AST::Less;
-            compound->expr1 = output;
-            compound->expr2 = this->parseExpr(tokens);
-            return compound;
-        } else if (sym.meta == ">"){
-            tokens.pop();
-            compound->op = AST::Great;
-            compound->expr1 = output;
-            compound->expr2 = this->parseExpr(tokens);
-            return compound;
-        }
-    }
     return output;
 }
