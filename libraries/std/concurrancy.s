d4b034a6147dce034b14be7a58d1e98e51a8bcce
DeForestt Thompson <deforesttthompson@gmail.com>
2021-11-08 12:59:26 -0500

    concurrancy update

--- libraries/std/concurrancy.s
+++ libraries/std/concurrancy.s
@@ -5,6 +5,8 @@
 .global	pub_Pipe_read
 .global	pub_Pipe_init
 .global	newProcess
+.global	pub_Process_isRunning
+.global	pub_Process_getPid
 .global	pub_Process_start
 .global	pub_Process_init
 
@@ -55,6 +57,38 @@ pub_Process_start:
 	movl	$0, %eax
 	leave
 	ret
+pub_Process_getPid:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	subq	$32, %rsp
+	movq	%rdi, -20(%rbp)
+	movq	-20(%rbp), %rdx
+	movl	0(%rdx), %eax
+	leave
+	ret
+pub_Process_isRunning:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	subq	$32, %rsp
+	movq	%rdi, -20(%rbp)
+	movq	-20(%rbp), %rdx
+	movl	0(%rdx), %eax
+	movl	%eax, %edi
+	movl	$0, %eax
+	movl	%eax, %esi
+	call	sys_kill
+	movl	%eax, -24(%rbp)
+	movl	-24(%rbp), %eax
+	movl	$0, %ecx
+	cmpl	%ecx, %eax
+	jge	.LisRunning1
+	movl	$0, %eax
+	leave
+	ret
+.LisRunning1:
+	movl	$1, %eax
+	leave
+	ret
 newProcess:
 	pushq	%rbp
 	movq	%rsp, %rbp
@@ -67,11 +101,11 @@ newProcess:
 	movq	-16(%rbp), %rax
 	movq	$0, %rcx
 	cmpq	%rcx, %rax
-	jne	.LnewProcess1
+	jne	.LnewProcess2
 	movq	$0, %rax
 	leave
 	ret
-.LnewProcess1:
+.LnewProcess2:
 	lea	-16(%rbp), %rax
 	movq	(%rax), %rax
 	movq	%rax, %rdi
@@ -86,16 +120,16 @@ pub_Pipe_init:
 	movq	%rsp, %rbp
 	subq	$32, %rsp
 	movq	%rdi, -16(%rbp)
-	lea	-24(%rbp), %rax
-	movq	%rax, -32(%rbp)
-	movq	-32(%rbp), %rax
+	lea	-16(%rbp), %rax
+	movq	%rax, -24(%rbp)
+	movq	-24(%rbp), %rax
 	movq	%rax, %rdi
 	call	sys_pipe
-	movq	-32(%rbp), %rdx
+	movq	-24(%rbp), %rdx
 	movq	-16(%rbp), %rdx
 	movl	0(%rdx), %ebx
 	movl	%ebx, 4(%rdx)
-	movq	-32(%rbp), %rdx
+	movq	-24(%rbp), %rdx
 	movq	-16(%rbp), %rdx
 	movl	4(%rdx), %ebx
 	movl	%ebx, 0(%rdx)
@@ -106,49 +140,49 @@ pub_Pipe_read:
 	pushq	%rbp
 	movq	%rsp, %rbp
 	subq	$48, %rsp
-	movq	%rdi, -16(%rbp)
-	movq	%rsi, -24(%rbp)
-	movl	%edx, -28(%rbp)
-	movq	-16(%rbp), %rdx
+	movq	%rdi, -24(%rbp)
+	movq	%rsi, -32(%rbp)
+	movl	%edx, -36(%rbp)
+	movq	-24(%rbp), %rdx
 	movl	4(%rdx), %ebx
-	movl	%ebx, -32(%rbp)
-	movl	-32(%rbp), %eax
+	movl	%ebx, -40(%rbp)
+	movl	-40(%rbp), %eax
 	movl	%eax, %edi
-	movq	-24(%rbp), %rax
+	movq	-32(%rbp), %rax
 	movq	%rax, %rsi
-	movl	-28(%rbp), %eax
+	movl	-36(%rbp), %eax
 	movl	%eax, %edx
 	call	sys_read
-	movb	%al, -36(%rbp)
-	movl	-36(%rbp), %eax
+	movb	%al, -44(%rbp)
+	movl	-44(%rbp), %eax
 	leave
 	ret
 pub_Pipe_write:
 	pushq	%rbp
 	movq	%rsp, %rbp
 	subq	$48, %rsp
-	movq	%rdi, -16(%rbp)
-	movq	%rsi, -24(%rbp)
-	movl	%edx, -28(%rbp)
-	movq	-16(%rbp), %rdx
+	movq	%rdi, -24(%rbp)
+	movq	%rsi, -32(%rbp)
+	movl	%edx, -36(%rbp)
+	movq	-24(%rbp), %rdx
 	movl	0(%rdx), %ebx
-	movl	%ebx, -32(%rbp)
-	movl	-32(%rbp), %eax
+	movl	%ebx, -40(%rbp)
+	movl	-40(%rbp), %eax
 	movl	%eax, %edi
-	movq	-24(%rbp), %rax
+	movq	-32(%rbp), %rax
 	movq	%rax, %rsi
-	movl	-28(%rbp), %eax
+	movl	-36(%rbp), %eax
 	movl	%eax, %edx
 	call	sys_write
-	movl	%eax, -36(%rbp)
-	movl	-36(%rbp), %eax
+	movl	%eax, -44(%rbp)
+	movl	-44(%rbp), %eax
 	leave
 	ret
 newPipe:
 	pushq	%rbp
 	movq	%rsp, %rbp
 	subq	$16, %rsp
-	movl	$8, %eax
+	movl	$16, %eax
 	movl	%eax, %edi
 	call	malloc
 	movq	%rax, -8(%rbp)
@@ -175,15 +209,18 @@ wait:
 	movq	%rsp, %rbp
 	subq	$16, %rsp
 	movq	%rdi, -8(%rbp)
-	movq	-8(%rbp), %rdx
-	movl	0(%rdx), %eax
-	movl	%eax, %edi
-	movq	$0, %rax
-	movq	%rax, %rsi
+	lea	-8(%rbp), %rax
+	movq	(%rax), %rax
+	movq	%rax, %rdi
+	call	pub_Process_getPid
+	movl	%eax, %eax
+	movl	%eax, %esi
 	movq	$0, %rax
 	movq	%rax, %rdx
 	movq	$0, %rax
 	movq	%rax, %rcx
+	movq	$0, %rax
+	movq	%rax, %r8
 	call	sys_wait
 	movl	$0, %eax
 	leave
