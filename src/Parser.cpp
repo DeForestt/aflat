c782e21121cfb6d802cd763cb4016cd440f49fdd
deforestt <deforesttthompson@gmail.com>
2022-02-04 15:42:20 -0500

    fixed logical errors

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -566,7 +566,9 @@ AST::Expr* parse::Parser::parseExpr(links::LinkedList<lex::Token*> &tokens){
             AST::Lambda * lambda = new AST::Lambda();
             lambda->function = new AST::Function();
             lambda->function->args = this->parseArgs(tokens, ',', '=');
-            if(dynamic_cast<lex::OpSym *>(tokens.peek()) != nullptr and dynamic_cast<lex::OpSym *>(tokens.peek())->Sym == '{'){
+            if ((dynamic_cast<lex::OpSym *>(tokens.peek()) == nullptr)) throw err::Exception("Line: " + std::to_string(tokens.peek()->lineCount) + " Need an > to start lambda");
+            if ((dynamic_cast<lex::OpSym *>(tokens.pop())->Sym != '>')) throw err::Exception("Line: " + std::to_string(tokens.peek()->lineCount) + " Need an > to start lambda");
+            if(dynamic_cast<lex::OpSym *>(tokens.peek()) != nullptr && dynamic_cast<lex::OpSym *>(tokens.peek())->Sym == '{'){
                 tokens.pop();
                 lambda->function->statment = this->parseStmt(tokens);
             } else this->parseStmt(tokens, true);
