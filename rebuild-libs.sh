36d9803ddcfa59bbd9e8c9706a0621bd324b1a19
DeForestt <deforesttthompson@gmail.com>
2022-04-15 14:04:33 -0400

    added script to rebuild all libs

---
+++ rebuild-libs.sh
@@ -0,0 +1,12 @@
+function aflat {
+    ./bin/main $1 $2
+}
+aflat ./libraries/std/src/Collections.af ./libraries/std/Collections.s
+aflat ./libraries/std/src/concurrency.af ./libraries/std/concurrency.s
+aflat ./libraries/std/src/DateTime.af ./libraries/std/DateTime.s
+aflat ./libraries/std/src/files.af ./libraries/std/fils.s
+aflat ./libraries/std/src/io.af ./libraries/std/io.s
+aflat ./libraries/std/src/math.af ./libraries/std/math.s
+aflat ./libraries/std/src/std.af ./libraries/std/std.s
+aflat ./libraries/std/src/strings.af ./libraries/std/strings.s
+aflat ./libraries/std/src/String.af ./libraries/std/String.s
