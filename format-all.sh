45314bc80900f3053480729ef4265779c0141187
DeForestt <deforesttthompson@gmail.com>
2022-08-03 14:37:09 -0400

    Adding formatting script

---
+++ format-all.sh
@@ -0,0 +1,13 @@
+STYLE='Google'
+
+function format {
+    clang-format $1 -style $STYLE -i
+}
+
+format ./include/Parser/*
+format ./include/CodeGenerator/*
+format ./include/*
+
+format ./src/*
+format ./src/Parser/*
+format ./src/CodeGenerator/*
