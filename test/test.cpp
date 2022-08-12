f9a2a7528f58ae691e2242d0dbfa0688bcda79ca
DeForestt <deforesttthompson@gmail.com>
2022-08-12 10:09:36 -0400

    test commit

--- test/test.cpp
+++ test/test.cpp
@@ -9,5 +9,5 @@ TEST_CASE( "Factorials are computed", "[factorial]" ) {
     REQUIRE( Factorial(1) == 1 );
     REQUIRE( Factorial(2) ==  2 );
     REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+    REQUIRE( Factorial(10) == 3628800 ); 
 }
