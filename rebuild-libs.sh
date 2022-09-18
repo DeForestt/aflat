f8f0590ea972d87fd1e64321f62f4a364041341d
deforestt <deforesttthompson@gmail.com>
2022-09-18 00:50:15 -0400

    add std-cmp.af to rebuild libs

--- rebuild-libs.sh
+++ rebuild-libs.sh
@@ -7,6 +7,7 @@ aflat ./libraries/std/src/DateTime.af ./libraries/std/DateTime.s
 aflat ./libraries/std/src/files.af ./libraries/std/files.s
 aflat ./libraries/std/src/io.af ./libraries/std/io.s
 aflat ./libraries/std/src/math.af ./libraries/std/math.s
+aflat ./libraries/std/src/std-cmp.af ./libraries/std/std-cmp.s
 aflat ./libraries/std/src/std.af ./libraries/std/std.s
 aflat ./libraries/std/src/strings.af ./libraries/std/strings.s
 aflat ./libraries/std/src/String.af ./libraries/std/String.s
