f198977a393d330d8525fa02c7d3669da7d32529
DeForestt Thompson <deforesttthompson@gmail.com>
2022-11-15 21:28:39 -0500

    Hotfix/fstring 0 fix (#79)
    * add 0
    
    * add 0
    
    * add hex

--- src/Scanner.cpp
+++ src/Scanner.cpp
@@ -1,6 +1,6 @@
 #include "Scanner.hpp"
 #include "Exceptions.hpp"
-
+#include <iostream>
 #include <ctype.h>
 
 LinkedList<lex::Token *> lex::Lexer::Scan(string input) {
@@ -27,6 +27,16 @@ LinkedList<lex::Token *> lex::Lexer::Scan(string input) {
         i++;
         sym->lineCount = lineCount;
         tokens << sym;
+      } else if (input[i] == '0' && input[i + 1] == 'x') {
+        i += 2;
+        auto intLit = new lex::INT();
+        intLit->value = "0x";
+        while (std::isxdigit(input[i])) {
+          intLit->value += input[i];
+          i++;
+        }
+        intLit->lineCount = lineCount;
+        tokens << intLit;
       } else {
         auto IntLit = new lex::INT();
         IntLit->value = input[i];
@@ -46,7 +56,7 @@ LinkedList<lex::Token *> lex::Lexer::Scan(string input) {
                                  " on line " + std::to_string(lineCount));
           }
 
-          auto *FloatLit = new lex::FloatLit();
+          auto FloatLit = new lex::FloatLit();
           FloatLit->value = IntLit->value;
           FloatLit->lineCount = lineCount;
           tokens << FloatLit;
