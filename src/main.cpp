398fc19467ccda640f8da21e7eb39de79f1e4ced
DeForestt <deforesttthompson@gmail.com>
2023-09-20 21:18:36 -0400

    add to main

--- src/main.cpp
+++ src/main.cpp
@@ -412,6 +412,7 @@ void runConfig(cfg::Config &config, const std::string &libPath, char pmode) {
   linker.insert(linker.begin(), libPath + "Utils_Result.s");
   linker.insert(linker.begin(), libPath + "Utils_Functions.s");
   linker.insert(linker.begin(), libPath + "Utils_Map.s");
+  linker.insert(linker.begin(), libPath + "Utils_Properties.s");
 
   // run gcc on the linkerList
   std::string linkerList = "";
@@ -429,7 +430,7 @@ void runConfig(cfg::Config &config, const std::string &libPath, char pmode) {
   }
 
   system(gcc.c_str());
-  linker.erase(linker.begin(), linker.begin() + 18);
+  linker.erase(linker.begin(), linker.begin() + 19);
 
   if (!config.asm_) for (auto &s : linker) {
     std::filesystem::remove(s);
