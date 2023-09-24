1650908347c99624082d31becd9b56c050a716b1
DeForestt <deforesttthompson@gmail.com>
2023-09-24 02:51:53 -0400

    add servers

--- libraries/std/src/request.c
+++ libraries/std/src/request.c
@@ -82,3 +82,70 @@ int _aflat_server_spinUp(short port, int requestSize, char* (*requestHandler)(ch
     }
     return 0;
 }
+
+// Server side C/C++ program to demonstrate Socket
+// programming
+
+#include <unistd.h>
+#define PORT 8080
+
+int _serve(int port, char *(*handler)(char *, void *), void * data){
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = { 0 };
+    char* hello = "Hello from server";
+  
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+  
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET,
+                   SO_REUSEADDR | SO_REUSEPORT, &opt,
+                   sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+  
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr*)&address,
+             sizeof(address))
+        < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket
+         = accept(server_fd, (struct sockaddr*)&address,
+                  (socklen_t*)&addrlen))
+        < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    valread = read(new_socket, buffer, 1024);
+    char * response = handler(buffer, data);
+
+    send(new_socket, response, strlen(response), 0);
+    // closing the connected socket
+    close(new_socket);
+    // closing the listening socket
+    shutdown(server_fd, SHUT_RDWR);
+    return 0;
+}
+
+int serve(int port, char *(*handler)(char *, void *), void * data){
+    while(1){
+        _serve(port, handler, data);
+    }
+    return 0;
+}
