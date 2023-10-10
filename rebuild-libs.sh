af0266634ea3b485f642bcee442f58b5ee8df22d
DeForestt <deforesttthompson@gmail.com>
2023-10-10 17:20:02 -0400

    first-commit

--- rebuild-libs.sh
+++ rebuild-libs.sh
@@ -29,4 +29,8 @@ aflat ./libraries/std/src/HTTP/Server.af ./libraries/std/HTTP_Server.s
 aflat ./libraries/std/src/HTTP/Endpoints.af ./libraries/std/HTTP_Endpoints.s
 aflat ./libraries/std/src/Web/Content.af ./libraries/std/Web_Content.s
 aflat ./libraries/std/src/Web/Content/Bind.af ./libraries/std/Web_Content_Bind.s
+aflat ./libraries/std/src/JSON.af ./libraries/std/JSON.s
+aflat ./libraries/std/src/JSON/Parse.af ./libraries/std/Parse.s
+mv ./libraries/std/Parse.s ./libraries/std/JSON_Parse.s
+aflat ./libraries/std/src/JSON/Property.af ./libraries/std/JSON_Property.s
 gcc -g -no-pie -S -o ./libraries/std/request.s ./libraries/std/src/request.c
