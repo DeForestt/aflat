589e029f79c18391a973c7befb4cae11ff880499
= <deforesttthompson@gmail.com>
2025-06-06 00:28:03 -0400

    add sub

--- include/CompilerUtils.hpp
+++ include/CompilerUtils.hpp
@@ -4,11 +4,9 @@
 namespace compilerutils {
 
 std::string buildCompileCmd(const std::string &srcPath,
-                            const std::string &destPath,
-                            bool debug);
+                            const std::string &destPath, bool debug);
 
 std::string buildLinkCmd(const std::string &output,
-                         const std::string &linkerList,
-                         bool debug);
+                         const std::string &linkerList, bool debug);
 
 }  // namespace compilerutils
