1518d1b5aec917dd1e7f35742f75a74077be46aa
DeForestt Thompson <deforesttthompson@gmail.com>
2025-06-09 13:05:18 -0400

    Add deep copy support for AST statements

---
+++ test/test_DeepCopy.cpp
@@ -0,0 +1,26 @@
+#include "Parser/Parser.hpp"
+#include "Scanner.hpp"
+#include "catch.hpp"
+#include "Parser/AST.hpp"
+
+TEST_CASE("deepCopy clones statements", "[deepcopy]") {
+  lex::Lexer l;
+  parse::Parser p;
+  auto tokens = l.Scan("return 1;");
+  tokens.invert();
+  ast::Statement *stmt = p.parseStmt(tokens, true);
+  auto *ret = dynamic_cast<ast::Return *>(stmt);
+  REQUIRE(ret != nullptr);
+
+  ast::Statement *copyStmt = ast::deepCopy(stmt);
+  auto *retCopy = dynamic_cast<ast::Return *>(copyStmt);
+  REQUIRE(retCopy != nullptr);
+  REQUIRE(retCopy != ret);
+  auto *origInt = dynamic_cast<ast::IntLiteral *>(ret->expr);
+  auto *copyInt = dynamic_cast<ast::IntLiteral *>(retCopy->expr);
+  REQUIRE(origInt != nullptr);
+  REQUIRE(copyInt != nullptr);
+  REQUIRE(copyInt->val == origInt->val);
+  copyInt->val = 2;
+  CHECK(origInt->val == 1);
+}
