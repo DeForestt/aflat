7953a3697833f0ec018a1d50efd6db78d8feedb9
DeForestt Thompson <deforesttthompson@gmail.com>
2025-07-20 02:35:57 -0400

    Update first steps with new stuff (#377)
    * updates
    
    * updates
    
    * updates
    
    * fix dec
    
    * fix dec
    
    * fix dec
    
    * update done

--- src/CodeGenerator/Statements.cpp
+++ src/CodeGenerator/Statements.cpp
@@ -128,6 +128,13 @@ asmc::File gen::CodeGenerator::GenArgs(ast::Statement *STMT,
         }
       }
 
+      if (parse::PRIMITIVE_TYPES.find(arg->type.typeName) ==
+              parse::PRIMITIVE_TYPES.end() &&
+          arg->ident != "my" && arg->type.typeName != "any" &&
+          arg->type.typeName.find("~") == std::string::npos) {
+        this->getType(arg->type.typeName, OutputFile);
+      }
+
       size = arg->type.size;
       arg->type.arraySize = 1;
       mov->from = this->intArgs[intArgsCounter].get(arg->type.size);
