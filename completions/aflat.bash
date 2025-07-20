8cc4e1479ddd3029c8fda17f2ba4758285a71076
DeForestt Thompson <deforesttthompson@gmail.com>
2025-07-20 09:28:47 -0400

    Add shell completion setup to install script (#378)
    * Install shell completion
    
    * first

---
+++ completions/aflat.bash
@@ -0,0 +1,17 @@
+_aflat_completion() {
+    local cur prev commands opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    commands="make build run test add file module install update clean"
+    opts="-o --output -c --config -n --name -d --debug -q --quiet -t --trace-alerts -U --update-deps -K --clean-deps -C --clean-cache -N --no-cache -L --lib -h --help"
+    if [[ ${cur} == -* ]]; then
+        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+        return 0
+    fi
+    if [[ ${COMP_CWORD} -eq 1 ]]; then
+        COMPREPLY=( $(compgen -W "${commands}" -- ${cur}) )
+        return 0
+    fi
+}
+complete -F _aflat_completion aflat
