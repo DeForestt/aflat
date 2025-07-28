5932c10465d9e754e4d47b62b3742d5815b956d5
DeForestt Thompson <deforesttthompson@gmail.com>
2025-07-28 15:54:53 -0400

    Add jobs flag to bash completion

--- completions/aflat.bash
+++ completions/aflat.bash
@@ -4,7 +4,7 @@ _aflat_completion() {
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
     commands="make build run test add file module install update clean"
-    opts="-o --output -c --config -n --name -d --debug -q --quiet -t --trace-alerts -U --update-deps -K --clean-deps -C --clean-cache -N --no-cache -L --lib -h --help"
+    opts="-o --output -c --config -n --name -d --debug -q --quiet -t --trace-alerts -U --update-deps -K --clean-deps -C --clean-cache -N --no-cache -j --jobs -L --lib -h --help"
     if [[ ${cur} == -* ]]; then
         COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
         return 0
