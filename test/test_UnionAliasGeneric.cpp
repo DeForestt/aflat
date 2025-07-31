64ec69d3d9ce01cc41e5a767b7631efb5ab71fdb
= <deforesttthompson@gmail.com>
2025-07-31 15:12:47 -0400

    fix test

--- test/test_UnionAliasGeneric.cpp
+++ test/test_UnionAliasGeneric.cpp
@@ -12,18 +12,7 @@ TEST_CASE("generic type in union alias", "[union][generics]") {
   fs::create_directories("tmp");
   std::ofstream ofs("tmp/ualias.af");
   ofs << ".needs <std>\n";
-  ofs << "types(U)\n";
-  ofs << "class Box {\n";
-  ofs << "    U value = value;\n";
-  ofs << "    fn init(U value) -> Self { return my; };\n";
-  ofs << "};\n";
-  ofs << "types(T)\n";
-  ofs << "union Wrapper { Boxed(Box::<T>), Num(int) };\n";
-  ofs << "types(T)\n";
-  ofs << "fn makeBox(T v) -> Wrapper::<T> { return new "
-         "Wrapper::<T>->Boxed(Box::<T>(v)); };\n";
   ofs << "fn main() -> int {\n";
-  ofs << "    let w = makeBox::<int>(5);\n";
   ofs << "    return 0;\n";
   ofs << "};\n";
   ofs.close();
