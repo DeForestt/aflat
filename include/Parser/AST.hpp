ed933e6edb9d48a8fa46f189fa59c079132db8a5
= <deforesttthompson@gmail.com>
2025-08-13 22:11:20 -0400

    fix formats

--- include/Parser/AST.hpp
+++ include/Parser/AST.hpp
@@ -160,7 +160,7 @@ class Type {
 
   Type() = default;
   Type(const std::string &typeName, const asmc::Size &size)
-      : typeName(typeName), size(size) {};
+      : typeName(typeName), size(size){};
 };
 
 class Arg {
