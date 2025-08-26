962a7312ea70b1fa42939c9f72847a9b6ab5b0b4
DeForestt Thompson <deforesttthompson@gmail.com>
2025-08-26 14:25:46 -0400

    Add rule-of-six implementations for containers

--- test/test_HashMap.cpp
+++ test/test_HashMap.cpp
@@ -12,3 +12,29 @@ TEST_CASE("HashMap insert and retrieve", "[hashmap]") {
   REQUIRE(map.size() == 0);
   REQUIRE_FALSE(map.contains("a"));
 }
+
+TEST_CASE("HashMap copy and move", "[hashmap]") {
+  HashMap<int *> map;
+  int a = 1, b = 2;
+  map.insert("a", &a);
+  map.insert("b", &b);
+
+  HashMap<int *> copy(map);
+  REQUIRE(copy.size() == map.size());
+  REQUIRE(*(copy.get("a")) == 1);
+  REQUIRE(*(copy.get("b")) == 2);
+
+  HashMap<int *> assigned;
+  assigned = map;
+  REQUIRE(assigned.size() == map.size());
+  REQUIRE(*(assigned.get("a")) == 1);
+
+  HashMap<int *> moved(std::move(map));
+  REQUIRE(moved.size() == 2);
+  REQUIRE(map.size() == 0);
+
+  HashMap<int *> moveAssigned;
+  moveAssigned = std::move(moved);
+  REQUIRE(moveAssigned.size() == 2);
+  REQUIRE(moved.size() == 0);
+}
--- test/test_LinkedList.cpp
+++ test/test_LinkedList.cpp
@@ -18,3 +18,27 @@ TEST_CASE("LinkedList invert", "[linkedlist]") {
   list.invert();
   REQUIRE(list.peek() == 1);
 }
+
+TEST_CASE("LinkedList copy and move", "[linkedlist]") {
+  links::LinkedList<int> list;
+  list.push(1);
+  list.push(2);
+
+  links::LinkedList<int> copy(list);
+  REQUIRE(copy.size() == list.size());
+  REQUIRE(copy.peek() == list.peek());
+
+  links::LinkedList<int> assigned;
+  assigned = list;
+  REQUIRE(assigned.size() == list.size());
+  REQUIRE(assigned.peek() == list.peek());
+
+  links::LinkedList<int> moved(std::move(list));
+  REQUIRE(moved.size() == 2);
+  REQUIRE(list.size() == 0);
+
+  links::LinkedList<int> moveAssigned;
+  moveAssigned = std::move(moved);
+  REQUIRE(moveAssigned.size() == 2);
+  REQUIRE(moved.size() == 0);
+}
