6e3f794edd80c5941d6f100453fd17975c7e7307
DeForestt Thompson <deforesttthompson@gmail.com>
2026-07-02 04:10:51 -0400

    fixed tag? (#494)

--- src/CompilerUtils.cpp
+++ src/CompilerUtils.cpp
@@ -1,6 +1,15 @@
 #include "CompilerUtils.hpp"
 
+#include <cstdlib>
+
 namespace compilerutils {
+
+std::string releaseCpuFlags() {
+  const char *portable = std::getenv("AFLAT_PORTABLE_OUTPUT");
+  if (portable != nullptr && portable[0] != '\0' && portable[0] != '0')
+    return "-march=x86-64 -mtune=generic";
+  return "-march=native";
+}
 
 std::string buildCompileCmd(const std::string &srcPath,
                             const std::string &destPath, bool debug) {
@@ -8,7 +17,7 @@ std::string buildCompileCmd(const std::string &srcPath,
   if (debug)
     flags = "-g -no-pie -z noexecstack -S -lefence ";
   else
-    flags = "-O3 -march=native -S -no-pie -z noexecstack ";
+    flags = "-O3 " + releaseCpuFlags() + " -S -no-pie -z noexecstack ";
   return "gcc " + flags + srcPath + " -o " + destPath;
 }
 
@@ -18,7 +27,7 @@ std::string buildLinkCmd(const std::string &output,
   if (debug)
     flags = "-O0 -g -no-pie -z noexecstack -o ";
   else
-    flags = "-O3 -march=native -no-pie -z noexecstack -o ";
+    flags = "-O3 " + releaseCpuFlags() + " -no-pie -z noexecstack -o ";
   return "gcc " + flags + output + " " + linkerList;
 }
 
--- test/test_CompilerUtils.cpp
+++ test/test_CompilerUtils.cpp
@@ -1,8 +1,16 @@
 #include "CompilerUtils.hpp"
 #include "catch.hpp"
 
+#include <cstdlib>
+
+namespace {
+void unsetPortableOutput() { unsetenv("AFLAT_PORTABLE_OUTPUT"); }
+void setPortableOutput() { setenv("AFLAT_PORTABLE_OUTPUT", "1", 1); }
+} // namespace
+
 TEST_CASE("compile command uses optimization when not debugging",
           "[compilerutils]") {
+  unsetPortableOutput();
   auto cmd = compilerutils::buildCompileCmd("a.c", "a.s", false);
   REQUIRE(cmd.find("-O3") != std::string::npos);
   REQUIRE(cmd.find("-march=native") != std::string::npos);
@@ -11,6 +19,7 @@ TEST_CASE("compile command uses optimization when not debugging",
 
 TEST_CASE("link command uses optimization when not debugging",
           "[compilerutils]") {
+  unsetPortableOutput();
   auto cmd = compilerutils::buildLinkCmd("a.out", "foo.s", false);
   REQUIRE(cmd.find("-O3") != std::string::npos);
   REQUIRE(cmd.find("-march=native") != std::string::npos);
@@ -18,7 +27,19 @@ TEST_CASE("link command uses optimization when not debugging",
 }
 
 TEST_CASE("debug flags omit optimizations", "[compilerutils]") {
+  unsetPortableOutput();
   auto cmd = compilerutils::buildLinkCmd("a.out", "foo.s", true);
   REQUIRE(cmd.find("-g") != std::string::npos);
   REQUIRE(cmd.find("-O3") == std::string::npos);
+}
+
+TEST_CASE("portable output avoids host native CPU flags", "[compilerutils]") {
+  setPortableOutput();
+  auto compileCmd = compilerutils::buildCompileCmd("a.c", "a.s", false);
+  auto linkCmd = compilerutils::buildLinkCmd("a.out", "foo.s", false);
+  REQUIRE(compileCmd.find("-march=native") == std::string::npos);
+  REQUIRE(linkCmd.find("-march=native") == std::string::npos);
+  REQUIRE(compileCmd.find("-march=x86-64") != std::string::npos);
+  REQUIRE(linkCmd.find("-mtune=generic") != std::string::npos);
+  unsetPortableOutput();
 }
